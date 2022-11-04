dd311a63e792562dcf36c244c65da560cefd48db
DerFetzer <kontakt@der-fetzer.de>
2022-11-04 13:09:49 +0100

    Add first test

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,19 @@ use std::error::Error;
 
 const DB_URL: &str = "sqlite:./sqlite.db?mode=rwc";
 
+fn app(db: DatabaseConnection) -> Router {
+    Router::new()
+        .route("/reports", post(add_report).get(get_reports))
+        .route("/reports/:id", get(get_single_report).delete(delete_report))
+        .route(
+            "/reports/:id/test_steps",
+            post(add_teststep).get(get_teststeps_for_report),
+        )
+        .route("/test_steps", get(get_teststeps))
+        .route("/test_steps/:id", get(get_single_teststep))
+        .layer(Extension(db))
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let db = Database::connect(DB_URL).await?;
@@ -27,16 +40,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     migrator::Migrator::up(&db, None).await?;
     assert!(schema_manager.has_table("report").await?);
 
-    let app = Router::new()
-        .route("/reports", post(add_report).get(get_reports))
-        .route("/reports/:id", get(get_single_report).delete(delete_report))
-        .route(
-            "/reports/:id/test_steps",
-            post(add_teststep).get(get_teststeps_for_report),
-        )
-        .route("/test_steps", get(get_teststeps))
-        .route("/test_steps/:id", get(get_single_teststep))
-        .layer(Extension(db));
+    let app = app(db);
 
     axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
         .serve(app.into_make_service())
@@ -75,9 +79,10 @@ async fn get_reports(
 async fn get_single_report(
     Path(report_id): Path<i32>,
     Extension(db): Extension<DatabaseConnection>,
-) -> Result<Json<Option<report::Model>>, StatusCode> {
+) -> Result<Json<report::Model>, StatusCode> {
     match Report::find_by_id(report_id).one(&db).await {
-        Ok(res) => Ok(Json(res)),
+        Ok(Some(res)) => Ok(Json(res)),
+        Ok(None) => Err(StatusCode::NOT_FOUND),
         Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
     }
 }
@@ -168,5 +173,137 @@ async fn delete_report(
             format!("Could not find report with id {}!", report_id),
         ),
         Err(_) => (StatusCode::INTERNAL_SERVER_ERROR, String::new()),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::http;
+    use axum::http::Request;
+    use hyper::Body;
+    use serde_json::json;
+    use temp_file::TempFile;
+    use tower::{Service, ServiceExt};
+
+    async fn setup_empty_temp_database() -> (DatabaseConnection, TempFile) {
+        let tmp_file = temp_file::empty();
+
+        let db = Database::connect(format!(
+            "sqlite:{}?mode=rwc",
+            tmp_file.path().to_str().unwrap()
+        ))
+        .await
+        .unwrap();
+        migrator::Migrator::fresh(&db).await.unwrap();
+        (db, tmp_file)
+    }
+
+    #[tokio::test]
+    async fn test_report() {
+        let (db, _tmp_file) = setup_empty_temp_database().await;
+        let mut app = app(db);
+
+        // Add report
+        app.call(
+            Request::builder()
+                .method(http::Method::POST)
+                .uri("/reports")
+                .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                .body(Body::from(
+                    serde_json::to_vec(&json!(
+                        {"date": "2014-11-28T21:00:09+09:00",
+                        "project": "TestProjekt",
+                        "name": "TestReport",
+                        "verdict": "PASSED"}))
+                    .unwrap(),
+                ))
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+        // Get report
+        let response = app
+            .ready()
+            .await
+            .unwrap()
+            .call(
+                Request::builder()
+                    .method(http::Method::GET)
+                    .uri("/reports/1")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        assert_eq!(
+            serde_json::from_slice::<serde_json::Value>(&body).unwrap(),
+            // TODO: Find the reason why the returned timestamp is in UTC
+            json!(
+                {
+                    "date":
+                    "2014-11-28T12:00:09+00:00",
+                    "id": 1,
+                    "project": "TestProjekt",
+                    "name": "TestReport",
+                    "verdict": "PASSED"
+            })
+        );
+
+        // Delete report
+        let response = app
+            .ready()
+            .await
+            .unwrap()
+            .call(
+                Request::builder()
+                    .method(http::Method::DELETE)
+                    .uri("/reports/1")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        assert_eq!(body, "");
+
+        let response = app
+            .ready()
+            .await
+            .unwrap()
+            .call(
+                Request::builder()
+                    .method(http::Method::DELETE)
+                    .uri("/reports/1")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        assert_eq!(body, "Could not find report with id 1!");
+
+        // Check that report got deleted
+        let response = app
+            .ready()
+            .await
+            .unwrap()
+            .call(
+                Request::builder()
+                    .method(http::Method::GET)
+                    .uri("/reports/1")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::NOT_FOUND);
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        assert_eq!(body, "");
     }
 }
